1f9072b63dff3b948a9c71f8556557898609ef2f
Carson <rodriguescarson@hotmail.com>
2022-03-04 21:05:39 +0530

    add: firbasefunctions - commented

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -9,10 +9,17 @@ export const locationRequest = (searchTerm) => {
         }
         resolve(locationMock)
     })
+    //firebase return
+    // return fetch(
+    //     `http://localhost:5001/mealstogo-bbcf1/us-central1/geocode/?city=${searchTerm}`
+    // ).then((res) => {
+    //     return res.json();
+    // });
 }
 
 
 export const locationTransform = (result) => {
+    console.log(result)
     const formattedResponse = camelize(result)
     const { geometry = {} } = formattedResponse.results[0];
     const { lat, lng } = geometry.location;
