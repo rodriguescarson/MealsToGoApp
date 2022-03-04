f61717712c300da61b4f1d0de4ca47f4959c707b
Carson <rodriguescarson@hotmail.com>
2022-03-04 22:20:01 +0530

    removed console.log

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -19,7 +19,6 @@ export const locationRequest = (searchTerm) => {
 
 
 export const locationTransform = (result) => {
-    console.log(result)
     const formattedResponse = camelize(result)
     const { geometry = {} } = formattedResponse.results[0];
     const { lat, lng } = geometry.location;
